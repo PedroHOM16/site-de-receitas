827d0dbc113cf42f8e82016f2eb5817a7b275123
Gabriela Papin <gabrielapapin20@gmail.com>
2022-04-29 16:43:28 -0300

    arrumei imagens do footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,78 +1,46 @@
-// import PropTypes from 'prop-types';
 import React from 'react';
-import '../images/drinkIcon.svg';
-import '../images/exploreIcon.svg';
-import '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   function submitTeste() {
-    // const { history } = props;
-    // console.log(history);
-    // history.push('/explore/drinks');
     window.location.href = '/drinks';
   }
   function submitTeste2() {
-    // const { history } = props;
-    // history.push('/explore');
     window.location.href = '/explore';
   }
   function submitTeste3() {
-    // const { history } = props;
-    // history.push('/explore/foods');
     window.location.href = '/foods';
   }
   return (
-    <div>
-      <footer data-testid="footer">
-        <button
-          className="button-img"
-          type="button"
+    <div className="container-footer">
+      <footer data-testid="footer" className="footer">
+        <input
           data-testid="drinks-bottom-btn"
-          src="drinkIcon.svg"
+          className="drinks-bottom-btn"
+          type="button"
+          src={ drinkIcon }
           onClick={ () => submitTeste() }
-        >
-          <img
-            src="drinkIcon.svg"
-            alt="drinks"
-          />
-
-        </button>
-        <button
-          className="button-img"
-          type="button"
+        />
+        <input
           data-testid="explore-bottom-btn"
-          src="exploreIcon.svg"
-          onClick={ () => submitTeste2() }
-        >
-          <img
-            src="exploreIcon.svg"
-            alt="explore"
-          />
-
-        </button>
-        <button
-          className="button-img"
+          className="explore-bottom-btn"
           type="button"
+          src={ exploreIcon }
+          onClick={ () => submitTeste2() }
+        />
+        <input
           data-testid="food-bottom-btn"
-          src="mealIcon.svg"
+          className="food-bottom-btn"
+          type="button"
+          src={ mealIcon }
           onClick={ () => submitTeste3() }
-        >
-          <img
-            src="mealIcon.svg"
-            alt="food"
-          />
-
-        </button>
-
+        />
       </footer>
     </div>
   );
 }
-
-// Footer.propTypes = {
-//   history: PropTypes.shape({
-//     push: PropTypes.func,
-//   }),
-// }.isRequired;
 
 export default Footer;
