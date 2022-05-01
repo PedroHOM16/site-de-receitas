2c8533dddffe05bcafefa102e2a88b7ebbdd68ad
Gabriela Papin <gabrielapapin20@gmail.com>
2022-04-30 22:28:47 -0300

    requisito 25 e 26 concluido

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CardRecipes from '../components/CardRecipes';
 import RecipesContext from '../context/RecipesContext';
+import { getDrinksByName } from '../services/ApiDrinks';
 
 function Drinks() {
-  const { filterDrinks } = useContext(RecipesContext);
+  const { filterDrinks, setFilterDrinks } = useContext(RecipesContext);
   const twelve = 12;
+
+  useEffect(() => {
+    const getDrink = async () => {
+      const data = await getDrinksByName('');
+      setFilterDrinks(data.drinks);
+    };
+    getDrink();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import CardRecipes from '../components/CardRecipes';
 import RecipesContext from '../context/RecipesContext';
+import { getMealByName } from '../services/ApiMeals';
 
 function Foods() {
-  const { filter } = useContext(RecipesContext);
+  const { filter, setFilter } = useContext(RecipesContext);
   const twelve = 12;
+
+  useEffect(() => {
+    const getFood = async () => {
+      const data = await getMealByName('');
+      setFilter(data.meals);
+    };
+    getFood();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
